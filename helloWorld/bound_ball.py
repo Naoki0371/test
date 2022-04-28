d66aa14b47ea2e348dafdb51e3282f74f91bab06
Naoki0371 <naoki_5736@yahoo.co.jp>
2022-04-28 13:49:36 +0900

    third

---
+++ helloWorld/bound_ball.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+ts = 0.01
+g = 9.8
+e = 0.6
+v = []
+p = []
+t = []
+v.append(0)
+p.append(10)
+t.append(0)
+for i in range(1, 1000):
+    t.append(ts)
+    v.append(v[i-1]-g*ts)
+    p.append(p[i-1]+v[i-1]*ts)
+    if p[i]<0:
+        p[i] = -e*p[i]
+        v[i] = -e*v[i]
+    plt.plot(t, p)
+    plt.show()
+    plt.close()
